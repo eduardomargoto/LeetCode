4531a85eb8d7a4d0d3a63464a5b258e56ff81cbf
Eduardo Margoto <eduardo.margoto@gmail.com>
2025-01-24 21:09:35 -0300

    improving Solution

--- 1071-greatest-common-divisor-of-stings/Solution1071.kt
+++ 1071-greatest-common-divisor-of-stings/Solution1071.kt
@@ -1,36 +1,12 @@
 fun gcdOfStrings(str1: String, str2: String): String {
-
-    var totalDivisor1 = 0
-    var totalDivisor2 = 0
-    for (i in str2.length downTo 1) {
-        val strToDivide = str2.substring(0, i)
-        for (n in 0 until str1.length step i) {
-            if (str1.length < n + i) break
-            val strToCompare = str1.substring(n, n + i)
-            if (strToCompare == strToDivide) {
-                totalDivisor1++
-            }
-        }
-
-        for (n in 0 until str2.length step i) {
-            if (str2.length < n + i) break
-            val strToCompare = str2.substring(n, n + i)
-            if (strToCompare == strToDivide) {
-                totalDivisor2++
-            }
-        }
-
-        if (strToDivide.length * totalDivisor1 == str1.length
-            && strToDivide.length * totalDivisor2 == str2.length
-        ) {
-            return strToDivide
-        }
-
-        totalDivisor1 = 0
-        totalDivisor2 = 0
+    if (str1 + str2 != str2 + str1) return ""
+    var str1Length = str1.length
+    var str2Length = str2.length
+    while (str1Length != str2Length) {
+        if (str1Length > str2Length) str1Length -= str2Length
+        else str2Length -= str1Length
     }
-
-    return ""
+    return str1.substring(0, str1Length)
 }
 
 fun main() {
